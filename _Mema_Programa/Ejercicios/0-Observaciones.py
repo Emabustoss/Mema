93745c1d405b64c849e9461cf19fcc70f4a72e1a
Emabustoss <121694272+Emabustoss@users.noreply.github.com>
2024-06-08 03:01:57 -0300

    Add files via upload

---
+++ _Mema_Programa/Ejercicios/0-Observaciones.py
@@ -0,0 +1,24 @@
+                    #STRING
+#""" BLA """: Puede alargar el texto por parrafo
+#len(): Mide la longitud de un argumento
+
+
+
+                    #LISTAS
+#append(): Agrega un elemento al final de la lista.
+#insert(): Inserta un elemento en una posición específica de la lista.
+#extend(): Agrega los elementos de otra lista al final de la lista actual.
+#index(): Devuelve el índice de la primera aparición de un elemento en la lista.
+#remove(): Elimina la primera aparición de un elemento de la lista.
+#sort(): Ordena los elementos de la lista en orden ascendente (por defecto) o descendente.
+#.pop():  Es utilizado en Python para eliminar y devolver el último elemento de una lista. 
+
+
+
+                    #Iterables 
+                    
+
+
+tupla = (1,2,3)
+a,_,c = tupla
+print(a,c)
